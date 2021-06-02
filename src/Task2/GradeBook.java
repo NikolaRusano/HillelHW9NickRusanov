611282d3bdeaa1242531bc53ea171d4fe8a96973
mrusanov <nikolarusano92@gmail.com>
2021-06-02 16:28:43 +0300

    2st task finished 2

--- src/Task2/GradeBook.java
+++ src/Task2/GradeBook.java
@@ -1,6 +1,7 @@
 package Task2;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class GradeBook {
@@ -16,7 +17,23 @@ public class GradeBook {
 
         HashMap<String, Integer> gradeBookMap = new HashMap<>();
 
-       firstTest.forEach((key,value) -> if(firstTest.values() > secondTest.values()) -> gradeBookMap.add(firstTest(key,value)));
+       //firstTest.forEach((key,value) -> if(firstTest.values() > secondTest.values()) -> gradeBookMap.add(firstTest(key,value)));
+
+       /* firstTest.forEach((key, value) -> (firstTest.get(key) > secondTest.get(key)) ? gradeBookMap.put(firstTest(key,value)) :
+                gradeBookMap.put(secondTest(key,value));*/
+
+
+
+        for (Map.Entry <String, Integer> rec : firstTest.entrySet()) {
+            if ((rec.getValue() > secondTest.get(rec.getKey()))) {
+                gradeBookMap.put(rec.getKey(), rec.getValue());
+            } else {
+                gradeBookMap.put(rec.getKey(), secondTest.get(rec.getKey()));
+            }
+        }
+
+        gradeBookMap.forEach((key,value) -> System.out.println(key + " " + value));
+
     }
 
 
